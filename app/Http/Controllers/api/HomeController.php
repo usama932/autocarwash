802215cf5fb4a7a4b13431307f8b8b46869c7949
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2022-12-30 12:19:50 +0500

    push

--- app/Http/Controllers/api/HomeController.php
+++ app/Http/Controllers/api/HomeController.php
@@ -4,8 +4,63 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Response;
 
 class HomeController extends Controller
 {
-    //
+    public function login(Request $request)
+        {
+            $data = $request->validate([
+                'email' => 'required|string',
+                'password' => 'required|string'
+            ]);
+
+            $user = User::where('email', $data['email'])->first();
+
+            if (!$user || !Hash::check($data['password'], $user->password)) {
+                return response([
+                    'msg' => 'incorrect username or password'
+                ], 401);
+            }
+
+            $token = $user->createToken('apiToken')->plainTextToken;
+
+            $res = [
+                'user' => $user,
+                'token' => $token
+            ];
+
+            return response()->json($res, 200);
+        }
+    public function sign_up(Request $request){
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+            'roled' => 'user'
+        ]);
+
+        $token = $user->createToken('apiToken')->plainTextToken;
+
+        $res = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response()->json($res, 200);
+    }
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'user logged out'
+        ];
+    }
 }
