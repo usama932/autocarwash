5c119bc415d10896b084be7ee91697a0fbf4728a
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-01-06 12:22:02 +0500

    Update contact.blade.php

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -83,9 +83,9 @@
                             </form>
                         </div>
                     </div>
-                    <div class="col-md-12">
-                        <iframe src="https://www.google.com/maps/place/Auto1+Car+Wash/@-33.9180489,150.9248444,17z/data=!3m1!4b1!4m5!3m4!1s0x6b12956b9f534851:0xa62242a23b9be04a!8m2!3d-33.9180489!4d150.9248444" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-                    </div>
+                    {{-- <div class="col-md-12">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1600663868074!5m2!1sen!2sbd" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div> --}}
                 </div>
             </div>
         </div>
