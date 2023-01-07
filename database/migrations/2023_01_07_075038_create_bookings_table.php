79e96dd0290a82f7395a20a6900ab5dafe9ea915
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-01-07 21:12:23 +0500

    push

---
+++ database/migrations/2023_01_07_075038_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->index('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('vehicle_type')->nullable();
+            $table->string('vehicle_no')->nullable();
+            $table->string('appointment_date')->nullable();
+            
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
