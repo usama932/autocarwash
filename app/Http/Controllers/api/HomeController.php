3b5de102b7050d6e3fafac8e276bc665a552cda2
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-02-11 23:58:27 +0500

    Update HomeController.php

--- app/Http/Controllers/api/HomeController.php
+++ app/Http/Controllers/api/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Reward;
 use Illuminate\Support\Facades\Hash;
 use Response;
 
