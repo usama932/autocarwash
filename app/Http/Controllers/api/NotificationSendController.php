598478f880fd0acd16554c05e857e5f5bfc0c05f
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-02-26 23:24:09 +0500

    Update NotificationSendController.php

--- app/Http/Controllers/api/NotificationSendController.php
+++ app/Http/Controllers/api/NotificationSendController.php
@@ -23,6 +23,46 @@ class NotificationSendController extends Controller
 
     public function sendNotification(Request $request)
     {
-       dd($request->all());
+      
+        $url = 'https://fcm.googleapis.com/fcm/send';
+    
+        $FcmToken = User::whereNotNull('device_token')->pluck('device_token')->all();
+            
+        $serverKey = 'AAAAvgFHeks:APA91bF2eIn-z3yE6NJ5M-fziz8Lu15BMqRZSbVXxlxxhP78ph8cQREVJESv_sl5e416Q_AERAi0dSKIHJVOYs14P2poTPqoG7kCbzg4MrG51b5SaPLbkDuiGhHniFe0dMEw2cqgLreD'; // ADD SERVER KEY HERE PROVIDED BY FCM
+    
+        $data = [
+            "registration_ids" => $FcmToken,
+            "notification" => [
+                "title" => $request->title,
+                "body" => $request->body,  
+            ]
+        ];
+        $encodedData = json_encode($data);
+    
+        $headers = [
+            'Authorization:key=' . $serverKey,
+            'Content-Type: application/json',
+        ];
+    
+        $ch = curl_init();
+        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        // Disabling SSL Certificate support temporarly
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+        // Execute post
+        $result = curl_exec($ch);
+        if ($result === FALSE) {
+            die('Curl failed: ' . curl_error($ch));
+        }        
+        // Close connection
+        curl_close($ch);
+        // FCM response
+        dd($result);
 }
 }
