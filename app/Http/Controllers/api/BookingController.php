567c404bb12443d2073b434c325767d98dc5050f
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-02-28 14:18:46 +0500

    push

--- app/Http/Controllers/api/BookingController.php
+++ app/Http/Controllers/api/BookingController.php
@@ -75,7 +75,7 @@ class BookingController extends Controller
          else{
             $discounted_price = ($totol_price * 10)/100;
          }
-       $total_price = $totol_price - $discounted_price ;
+         $totol_price = $totol_price - $discounted_price;
        $booking = Bookings::create([
         'user_id'           => auth()->user()->id,
         'user'              => auth()->user()->name,
