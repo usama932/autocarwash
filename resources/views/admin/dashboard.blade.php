63926134a05cc3e3cccc72639bcbcededef00004
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-03-02 15:56:58 +0500

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
 
     <!-- Content Row -->
     <div class="row">
-    @if(auth()->roled == 'admin')
+    @if(auth()->user->roled == 'admin')
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
